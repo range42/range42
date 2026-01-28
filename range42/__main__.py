d924c5c085c593a81a89d782b1ea347a6315de32
t0kubetsu <alexis.danjon@nc3.lu>
2026-01-28 18:25:35 +0100

    supress urllib3 warning and verbose logs

--- range42/__main__.py
+++ range42/__main__.py
@@ -1,11 +1,14 @@
 import logging
+import warnings
+
+from urllib3.exceptions import InsecureRequestWarning
 
 from .config import Config
 from .preparator import Preparator
 
 
 def main():
-    config = Config()
+    config = Config.from_env()
     logging.debug(f"Variables configured:\n\t{config}")
     try:
         Preparator(config)
@@ -18,5 +21,7 @@ if __name__ == "__main__":
         level=logging.DEBUG,
         format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
     )
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    warnings.simplefilter("ignore", InsecureRequestWarning)
 
     main()
